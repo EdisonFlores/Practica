f4d3f6a0c86cbce7d6ef552592547a57ba486645
EdisonFlores <edison.flores@espoch.edu.ec>
2021-07-30 22:39:28 -0500

    Modificación 3

--- Archivo1.py
+++ Archivo1.py
@@ -1,3 +1,4 @@
 print("Grupo4")
 print("Practica de laboratorio Uso de Git y GitHub")
 print("Integrantes:")
+print ("Edison Flores,Howard Salas,Bryan Rivadeneira")
--- Archivo2.py
+++ Archivo2.py
@@ -1,2 +1,3 @@
 print("Versión 1")
 print("Versión2")
+print("Versión 3")
